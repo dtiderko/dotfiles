92edac3f70949d0e0c8ca9d76d03921f799d0c9c
Gooxey <dennis.tiderko@gmail.com>
2025-03-25 15:42:39 +0100

    default keybinds of coq.nvim are good

--- home-manager/nixvim/config/plugins/coq-nvim.nix
+++ home-manager/nixvim/config/plugins/coq-nvim.nix
@@ -1,21 +1,5 @@
 {
   enable = true;
   installArtifacts = true;
-  settings = {
-    auto_start = "shut-up";
-    keymap.recommended = false;
-  };
-  luaConfig.post = ''
-    vim.api.nvim_set_keymap('i', '<Esc>', [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<C-c>', [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<BS>', [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap(
-      "i",
-      "<Space>",
-      [[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"]],
-      { expr = true, silent = true }
-    )
-    vim.api.nvim_set_keymap('i', '<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<BS>"]], { expr = true, silent = true })
-  '';
+  settings.auto_start = "shut-up";
 }
