ffc18197b443742c4b735c5feb3518f4ae54fe8f
Gooxey <dennis.tiderko@gmail.com>
2025-04-16 16:25:14 +0200

    cosmic now has an official package!

--- desktop-environment/gnome-cosmic/default.nix
+++ desktop-environment/gnome-cosmic/default.nix
@@ -1,14 +1,5 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 {
-  imports = [
-    inputs.nixos-cosmic.nixosModules.default
-  ];
-
-  nix.settings = {
-    substituters = [ "https://cosmic.cachix.org/" ];
-    trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-  };
-
   services = {
     # cosmic
     desktopManager.cosmic.enable = true;
