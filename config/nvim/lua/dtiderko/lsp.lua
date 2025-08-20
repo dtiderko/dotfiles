779c7f893837b88bcfc3281b9916c469e6f76e8e
Gooxey <dennis.tiderko@gmail.com>
2025-08-20 14:23:01 +0200

    nvim: lua will now load nvims libraries

--- config/nvim/lua/dtiderko/lsp.lua
+++ config/nvim/lua/dtiderko/lsp.lua
@@ -103,8 +103,8 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
-			diagnostics = {
-				globals = { "vim" },
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
 			},
 		},
 	},
