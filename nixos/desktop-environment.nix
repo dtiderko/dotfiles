6d44e3edb8a44f48c2f3ecb6bc579c8ebb935124
dtiderko <dennis@tiderko.de>
2025-10-11 14:01:27 +0200

    fix the stupid clipboard on wayland

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -4,4 +4,7 @@
     xserver.displayManager.gdm.enable = true;
     desktopManager.cosmic.enable = true;
   };
+
+  # fix the stupid clipboard
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 }
