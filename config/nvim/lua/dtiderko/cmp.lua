e0341ad29ae093cc70c89ea9380f0eecc22a72d8
dtiderko <dennis@tiderko.de>
2026-03-12 20:27:47 +0100

    neovim: dont autocomplete on space

--- config/nvim/lua/dtiderko/cmp.lua
+++ config/nvim/lua/dtiderko/cmp.lua
@@ -40,7 +40,7 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 
 		["<CR>"] = cmp.mapping.confirm({ select = false }),
-		["<Space>"] = cmp.mapping.confirm({ select = false }),
+		-- ["<Space>"] = cmp.mapping.confirm({ select = false }),
 
 		["<C-u>"] = cmp.mapping.scroll_docs(-4),
 		["<C-d>"] = cmp.mapping.scroll_docs(4),
