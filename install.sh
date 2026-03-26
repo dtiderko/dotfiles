aacb6f1cf558d997f32c007ef8034f8e37762075
dtiderko <dennis@tiderko.de>
2026-03-26 21:39:46 +0100

    install.sh: remove emacs specific code

--- install.sh
+++ install.sh
@@ -21,11 +21,6 @@ for dir in *; do
 done
 cd ..
 
-# install emacs dir
-sudo rm -rf ~/.emacs.d
-sudo ln -s "$(realpath emacs.d)" ~/.emacs.d
-UNINSTALL_SCRIPT="$UNINSTALL_SCRIPT; rm ~/.emacs.d"
-
 # some bash stuff to add
 if [ ! -f old_bashrc ]; then
   if [ ! -f ~/.bashrc ]; then
