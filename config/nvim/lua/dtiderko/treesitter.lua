e89731eb49b45dce36132327dcbde55c496a2523
dtiderko <dennis@tiderko.de>
2026-07-31 23:39:35 +0200

    nvim: fix treesitter

--- config/nvim/lua/dtiderko/treesitter.lua
+++ config/nvim/lua/dtiderko/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { load = true })
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter").setup({
 	auto_install = true,
 	highlight = {
 		enable = true,
